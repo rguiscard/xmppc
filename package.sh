c5f5ac888d653e0827203d0a174ed008d2c166a7
Home <nnee@edsfds.rr>
2023-11-11 16:22:40 +0000

    fix bug create deb in debian 11

--- package.sh
+++ package.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export TERM=xterm
 
+# Deleting everything inside the folder and the folder itself
+rm -rf ./deb/usr
+
 # Creating necessary directories
 mkdir -p ./deb/usr/local/xmppc
 mkdir -p ./deb/usr/bin
@@ -11,7 +14,7 @@ cp ./xmppc ./deb/usr/local/xmppc/xmppc
 chmod 755 ./deb/usr/local/xmppc/xmppc
 
 # Creating a symbolic link
-ln -s /usr/local/xmppc/xmppc /usr/bin/xmppc
+ln -s /usr/local/xmppc/xmppc ./deb/usr/bin/xmppc
 
 # Generating MD5
 md5deep -l -o f -r ./deb/usr > ./deb/DEBIAN/md5sums
